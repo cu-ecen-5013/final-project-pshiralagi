6f6b8c738299c1446a2f07eb12f41ac9128ec65f
Pavan <pash6214@colorado.edu>
2020-11-06 21:36:25 -0700

    Working build for raspberry pi, includes untested accelerometer package

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 78050c956cc18e644d84dc64cc3d1ee7b8aa7201
+AESD_ASSIGNMENTS_VERSION = 65c257f72130370668663310d2f3aa50ff1040c6
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -20,8 +20,8 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/usr/bin/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/usr/bin/conf/
-	$(INSTALL) -m 0755 $(@D)/adxl335 $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/adxl335-start-stop $(TARGET_DIR)/etc/init.d/S99adxl335
+	$(INSTALL) -m 0755 $(@D)/adxl335/adxl335 $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/adxl-start-stop.sh $(TARGET_DIR)/etc/init.d/S99adxl335
 endef
 
 $(eval $(generic-package))
